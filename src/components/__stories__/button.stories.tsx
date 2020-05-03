d03a2caeffb984aba2784650c87f369cdc87d4c2
Sebastian Cabeza <sebastian.cabeza@noted.com>
2020-05-04 09:01:47 +1200

    Test mode with a story

--- src/components/__stories__/button.stories.tsx
+++ src/components/__stories__/button.stories.tsx
@@ -2,6 +2,7 @@ import * as React from "react";
 import { storiesOf } from "@storybook/react";
 import { Button, PrimaryButton } from "../button";
 import { Box } from "../box";
+import { useTheme } from "../../theme";
 
 storiesOf("Buttons", module)
   .add("Default", () => (
@@ -13,4 +14,14 @@ storiesOf("Buttons", module)
     <PrimaryButton width="250px">
       <Box bg="green">{String("Test")}</Box>
     </PrimaryButton>
+  ))
+  .add("Mode", () => (
+    <PrimaryButton width="250px">
+      <MyStory />
+    </PrimaryButton>
   ));
+
+const MyStory = () => {
+  const { mode } = useTheme();
+  return <div>My current mode is: {mode}</div>;
+};
