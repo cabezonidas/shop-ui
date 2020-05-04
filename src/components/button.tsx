eecb978f17ec3e6fcef704b3bb5fcf673a13c765
Sebastian Cabeza <sebastian.cabeza@noted.com>
2020-05-05 09:18:29 +1200

    Delete primary button. Add variants

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -1,18 +1,67 @@
 import Box from "./box";
 import styled from "../theme/styled";
+import contrast from "../helpers/contrast";
+import { IColorTheme } from "../theme/colors";
+import invertColorTheme from "../helpers/invertColorTheme";
 
-export const Button = Box.withComponent("button");
+type ButtonVariant = "primary" | "secondary" | "default" | "info" | "warning" | "danger";
+
+export const Button = styled(Box.withComponent("button"))<{ variant?: ButtonVariant }>(
+  ({ theme, variant = "default" }) => {
+    const { colors } = theme;
+
+    const colorGrade =
+      variant !== "default" ? (colors[variant] as IColorTheme) : invertColorTheme(colors.neutral);
+
+    const textColor = (bg: string) =>
+      contrast(bg) === "light" ? colors.neutral.lightest : colors.neutral.darkest;
+
+    return {
+      backgroundColor: colorGrade.mediumDark,
+      color: textColor(colorGrade.mediumDark),
+
+      outlineColor: colorGrade.medium,
+      "&:hover": {
+        backgroundColor: colorGrade.dark,
+        color: textColor(colorGrade.dark),
+        outlineColor: colorGrade.medium,
+      },
+      "&:focus": {
+        backgroundColor: colorGrade.light,
+        color: textColor(colorGrade.light),
+        outlineColor: colorGrade.medium,
+        zIndex: 1,
+      },
+      "&:active": {
+        backgroundColor: colorGrade.mediumDark,
+        color: textColor(colorGrade.mediumDark),
+        outlineColor: colorGrade.dark,
+      },
+      "&:disabled": {
+        backgroundColor: colorGrade.lightest,
+        color: textColor(colorGrade.lightest),
+        outlineColor: colorGrade.medium,
+        cursor: "not-allowed",
+      },
+      appearance: "none",
+      cursor: "pointer",
+      display: "inline-block",
+      verticalAlign: "middle",
+      textOverflow: "ellipsis",
+      whiteSpace: "nowrap",
+      WebkitFontSmoothing: "subpixel-antialiased",
+      userSelect: "none",
+      WebkitTouchCallout: "none",
+      transition: `border-color 0.25s ease-in-out, box-shadow 0.1s ease-in-out background-color 0.25s ease-in-out, color 0.25s ease-in-out;`,
+    };
+  }
+);
 
 Button.defaultProps = {
   py: 2,
   px: 4,
   borderRadius: 4,
+  type: "button",
 };
-
-export const PrimaryButton = styled(Button)(
-  ({ theme }) => `
-  background-color: ${theme.colors.primary.medium};
-`
-);
 
 export default Button;
