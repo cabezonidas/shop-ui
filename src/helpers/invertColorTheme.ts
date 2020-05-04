c65dc2652d819caebef0d64ad2ca522f28c85ee4
Sebastian Cabeza <sebastian.cabeza@noted.com>
2020-05-05 09:17:14 +1200

    Add helper functions

---
+++ src/helpers/invertColorTheme.ts
@@ -0,0 +1,18 @@
+import { IColorTheme } from "../theme/colors";
+
+export default function invertColorTheme(colorGrade: IColorTheme): IColorTheme {
+  console.log({ colorGrade, keys: Object.keys(colorGrade) });
+  const grades: Array<{ grade: string; value: string }> = Object.keys(colorGrade).map(
+    (grade: string) => ({
+      grade,
+      value: colorGrade[grade] as string,
+    })
+  );
+  return grades.reduce(
+    (res, item, i) => ({
+      ...res,
+      [grades[grades.length - i - 1].grade]: item.value,
+    }),
+    { ...colorGrade }
+  );
+}
