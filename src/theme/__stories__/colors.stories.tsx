d7369e4c6b46c164cab4b00685348fb692edd523
Sebastian Cabeza <sebastian.cabeza@noted.com>
2020-05-05 09:18:13 +1200

    Update story. Use helper function

--- src/theme/__stories__/colors.stories.tsx
+++ src/theme/__stories__/colors.stories.tsx
@@ -3,7 +3,7 @@ import { theme } from "../theme";
 import { Box } from "../..";
 import { IColorTheme } from "../colors";
 import { storiesOf } from "@storybook/react";
-import { background } from "styled-system";
+import contrast from "../../helpers/contrast";
 
 storiesOf("Palette", module).add("Colors", () => <Colors />);
 
@@ -38,7 +38,7 @@ const Colors = () => {
                   borderRadius="2"
                   p="2"
                   width={100}
-                  color={contrast(hex)}
+                  color={contrast(hex) === "dark" ? "#000000" : "#ffffff"}
                 >
                   <Box>{grade}</Box>
                   <Box>{hex}</Box>
@@ -51,21 +51,3 @@ const Colors = () => {
     </Box>
   );
 };
-
-function contrast(hex: string) {
-  if (hex.indexOf("#") === 0) {
-    hex = hex.slice(1);
-  }
-  // convert 3-digit hex to 6-digits.
-  if (hex.length === 3) {
-    hex = hex[0] + hex[0] + hex[1] + hex[1] + hex[2] + hex[2];
-  }
-  if (hex.length !== 6) {
-    throw new Error("Invalid HEX color.");
-  }
-  const r = parseInt(hex.slice(0, 2), 16);
-  const g = parseInt(hex.slice(2, 4), 16);
-  const b = parseInt(hex.slice(4, 6), 16);
-
-  return r * 0.299 + g * 0.587 + b * 0.114 > 186 ? "#000000" : "#FFFFFF";
-}
