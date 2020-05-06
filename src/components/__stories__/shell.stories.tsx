100606a5c3f510265f73d83f041d7c7e70e5c8eb
Sebastian Cabeza <sebastian.cabeza@noted.com>
2020-05-07 08:01:38 +1200

    Update shell story

--- src/components/__stories__/shell.stories.tsx
+++ src/components/__stories__/shell.stories.tsx
@@ -2,4 +2,4 @@ import * as React from "react";
 import { storiesOf } from "@storybook/react";
 import Shell from "../shell";
 
-storiesOf("Shell", module).add("Landing area", () => <Shell bg="papayawhip" />);
+storiesOf("Shell", module).add("Landing area", () => <Shell>Welcome to kiwi UI</Shell>);
