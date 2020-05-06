d75455061f8bd082d123d72700e3dad220706082
Sebastian Cabeza <sebastian.cabeza@noted.com>
2020-05-07 08:01:27 +1200

    Remove margin from stories config

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,7 +1,6 @@
 import * as React from "react";
 import { configure, addDecorator } from "@storybook/react";
 import { UiProvider } from "../src/theme/ui-provider";
-import { Global, css } from "@emotion/core";
 import { useDarkMode } from "storybook-dark-mode";
 import { addParameters } from "@storybook/react";
 import "../src/public/style.css";
@@ -15,17 +14,6 @@ configure(() => {
       current: "light",
     },
   });
-  addDecorator(story => (
-    <UiProvider mode={useDarkMode() ? "dark" : "light"}>
-      <Global
-        styles={css`
-          body {
-            margin: 10px;
-          }
-        `}
-      />
-      {story()}
-    </UiProvider>
-  ));
+  addDecorator(story => <UiProvider mode={useDarkMode() ? "dark" : "light"}>{story()}</UiProvider>);
   req.keys().forEach(filename => req(filename));
 }, module);
