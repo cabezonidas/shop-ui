54000df12e495aa9b7ca1c5dd9ac39b8f58f738e
Sebastian Cabeza <sebastian.cabeza@noted.com>
2020-05-08 08:56:15 +1200

    Delete input story

--- src/components/__stories__/input.stories.tsx
+++
@@ -1,5 +0,0 @@
-import * as React from "react";
-import { storiesOf } from "@storybook/react";
-import { Input } from "..";
-
-storiesOf("Input", module).add("Default", () => <Input width="200px" />);
