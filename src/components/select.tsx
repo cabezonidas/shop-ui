ca1a9b27741b8cc70cd101bf7c0b787f6b5930a0
Sebastian Cabeza <sebastian.cabeza@noted.com>
2020-05-08 06:47:03 +1200

    Styled select

--- src/components/select.tsx
+++ src/components/select.tsx
@@ -1,12 +1,15 @@
 import Input from "./input";
+import { styled } from "../theme";
 
-export const Select = Input.withComponent("select");
+export const Select = styled(Input)(({ theme: { colors: { neutral }, space } }) => ({
+  backgroundImage: `url("data:image/svg+xml,${encodeURI(
+    `<svg xmlns="http://www.w3.org/2000/svg" width="10" height="10" viewBox="0 0 8 16" fill="${neutral.mediumDark}" stroke="${neutral.mediumDark}"><path d="M4 0L0 7H8L4 0Z" /> <path d="M4 16L8 9H0L4 16Z" /></svg>`
+  ).replace(/#/g, "%23")}")`,
+  backgroundRepeat: "no-repeat",
+  backgroundPosition: `right ${space[2]} center`,
+  backgroundSize: `${space[3]} ${space[3]}`,
+})).withComponent("select");
 
-Select.defaultProps = {
-  p: 2,
-  mb: 4,
-  borderRadius: 4,
-  width: "100%",
-};
+Select.displayName = "Select";
 
 export default Select;
