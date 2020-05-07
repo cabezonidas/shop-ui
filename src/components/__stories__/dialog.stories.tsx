aa247ee6269568d2abf7be2328e17fc5353e5a95
Sebastian Cabeza <sebastian.cabeza@noted.com>
2020-05-07 12:23:31 +1200

    Styled dialog

--- src/components/__stories__/dialog.stories.tsx
+++ src/components/__stories__/dialog.stories.tsx
@@ -2,8 +2,14 @@ import * as React from "react";
 import { storiesOf } from "@storybook/react";
 import { Dialog } from "../dialog";
 import Button from "../button";
+import { Box, Label } from "..";
+import Form from "../form";
+import Input from "../input";
 
-storiesOf("Dialog", module).add("From button", () => <Example />);
+// tslint:disable: jsx-use-translation-function
+storiesOf("Dialog", module)
+  .add("From button", () => <Example />)
+  .add("Dialog content", () => <Example2 />);
 
 function Example() {
   const [showDialog, setShowDialog] = React.useState(false);
@@ -18,9 +24,50 @@ function Example() {
           ea reprehenderit quis dolores temporibus commodi! Tempore esse voluptate earum corporis et
           quis rem ipsa dicta tenetur molestiae?
         </p>
-        <Button onClick={close} variant="primary">
-          Close
-        </Button>
+        <Box display="flex" justifyContent="flex-end" mt="4">
+          <Button onClick={close} variant="primary">
+            Close
+          </Button>
+        </Box>
+      </Dialog>
+    </div>
+  );
+}
+function Example2() {
+  const [showDialog, setShowDialog] = React.useState<"body" | "header" | "footer" | "all">(
+    undefined
+  );
+  const close = () => setShowDialog(undefined);
+
+  const [width, setWidth] = React.useState(50);
+  const [height, setHeight] = React.useState(50);
+
+  return (
+    <div>
+      <Button onClick={() => setShowDialog("body")}>Body only</Button>
+      <Dialog isOpen={showDialog === "body"} onDismiss={close}>
+        This is my content
+      </Dialog>
+      <Button onClick={() => setShowDialog("header")}>{"Body & Header"}</Button>
+      <Dialog isOpen={showDialog === "header"} onDismiss={close} header={"This is a header"}>
+        This is my content
+      </Dialog>
+      <Button onClick={() => setShowDialog("footer")}>Body & footer</Button>
+      <Dialog
+        isOpen={showDialog === "footer"}
+        onDismiss={close}
+        footer={<Button onClick={close}>Close</Button>}
+      >
+        This is my content
+      </Dialog>
+      <Button onClick={() => setShowDialog("all")}>Body & header & footer</Button>
+      <Dialog
+        isOpen={showDialog === "all"}
+        onDismiss={close}
+        header={"This is a header"}
+        footer={<Button onClick={close}>Close</Button>}
+      >
+        This is my content
       </Dialog>
     </div>
   );
--- src/components/dialog.tsx
+++ src/components/dialog.tsx
@@ -1,34 +1,82 @@
 import * as React from "react";
 import { Dialog as ReachDialog } from "@reach/dialog";
-import "@reach/dialog/styles.css";
 import { useTheme } from "../theme";
+import { Box, Button } from ".";
+import { Close } from "../icons";
+import "@reach/dialog/styles.css";
+import { useTranslation } from "react-i18next";
 
-export const Dialog = React.forwardRef<HTMLDivElement, React.ComponentProps<typeof ReachDialog>>(
-  (props, ref) => {
-    const { isOpen, ...otherProps } = props;
+interface IDialogProps extends React.ComponentProps<typeof ReachDialog> {
+  header?: React.ReactNode;
+  footer?: React.ReactNode;
+  containerProps?: React.ComponentProps<typeof Box>;
+}
 
-    const { open, style } = useFade(isOpen);
-    const {
-      mode,
-      colors: { neutral },
-    } = useTheme();
+const enUsDialog = { ui: { dialog: { close: "Close dialog" } } };
+const esArDialog = { ui: { dialog: { close: "Cerrar diálogo" } } };
 
-    return (
-      <ReachDialog
-        style={{
-          ...style,
-          borderRadius: "4px",
-          transition: "transform 0.15s, opacity 0.1s",
-          color: mode === "dark" ? neutral.lightest : neutral.darkest,
-          backgroundColor: mode === "dark" ? neutral.darkest : neutral.lightest,
-        }}
-        isOpen={open}
-        {...otherProps}
-        ref={ref}
-      />
-    );
-  }
-);
+export const Dialog = React.forwardRef<HTMLDivElement, IDialogProps>((props, ref) => {
+  const { isOpen, header, footer, children, containerProps, ...otherProps } = props;
+  const { onDismiss } = props;
+  const { open, style } = useFade(isOpen);
+  const gridTemplateRows =
+    header && footer ? "auto 1fr auto" : header ? "auto 1fr" : footer ? "1fr auto" : "1fr";
+
+  const {
+    mode,
+    colors: { neutral },
+  } = useTheme();
+
+  const { t, i18n } = useTranslation();
+  i18n.addResourceBundle("en-US", "translation", enUsDialog, true, true);
+  i18n.addResourceBundle("es-AR", "translation", esArDialog, true, true);
+
+  return (
+    <ReachDialog
+      style={{
+        ...style,
+        borderRadius: "4px",
+        transition: "transform 0.15s, opacity 0.1s",
+        color: mode === "dark" ? neutral.lightest : neutral.darkest,
+        backgroundColor: mode === "dark" ? neutral.darkest : neutral.lightest,
+        maxWidth: "90%",
+        width: "min-content",
+        padding: 0,
+      }}
+      isOpen={open}
+      {...otherProps}
+      ref={ref}
+    >
+      <Box
+        display="grid"
+        p="6"
+        maxWidth="100%"
+        width={["300px", "500px", "750px", "1000px"]}
+        minHeight="200px"
+        gridTemplateRows={gridTemplateRows}
+        gridGap="4"
+        {...containerProps}
+      >
+        {header && (
+          <Box display="grid" gridTemplateColumns="1fr auto" gridGap="2">
+            <Box>{header}</Box>
+            <Box>
+              <Button variant="transparent" onClick={onDismiss} aria-label={t("ui.dialog.close")}>
+                <Close />
+              </Button>
+            </Box>
+          </Box>
+        )}
+        {children}
+        {footer && (
+          <Box display="flex" justifyContent="flex-end">
+            {footer}
+          </Box>
+        )}
+      </Box>
+    </ReachDialog>
+  );
+});
 
 const useFade = (isOpen: boolean) => {
   const [style, setStyle] = React.useState<React.CSSProperties>({});
@@ -37,26 +85,28 @@ const useFade = (isOpen: boolean) => {
   React.useEffect(() => {
     let alive = true;
     const myOpen = !!isOpen;
-    if (!myOpen) {
+    let timeout: NodeJS.Timeout;
+    if (myOpen) {
+      setOpen(myOpen);
+      timeout = setTimeout(() => {
+        if (alive) {
+          setStyle({
+            opacity: 1,
+            transform: "translateY(5px)",
+          });
+        }
+      }, 150);
+    } else {
+      timeout = setTimeout(() => {
+        if (alive) {
+          setOpen(myOpen);
+        }
+      }, 150);
       setStyle({
         opacity: 0,
         transform: "translateY(-5px)",
       });
     }
-    const timeout = setTimeout(
-      () => {
-        if (alive) {
-          setOpen(myOpen);
-          if (myOpen) {
-            setStyle({
-              opacity: 1,
-              transform: "translateY(5px)",
-            });
-          }
-        }
-      },
-      myOpen ? 0 : 150
-    );
     return () => {
       alive = false;
       clearTimeout(timeout);
