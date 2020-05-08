103f5b073994dc2c7f011113f64a0fdd39fe13fa
Sebastian Cabeza <sebastian.cabeza@noted.com>
2020-05-09 08:34:40 +1200

    Update color palette

--- src/theme/colors.ts
+++ src/theme/colors.ts
@@ -10,67 +10,72 @@ export interface IColorTheme {
 
 export const primary: IColorTheme = {
   darkest: "#512020",
-  dark: "#632828",
-  mediumDark: "#762f2f",
-  medium: "#883636",
-  mediumLight: "#9a3d3d",
+  dark: "#5f2525",
+  mediumDark: "#6c2b2b",
+  medium: "#7a3030",
+  mediumLight: "#883636",
   light: "#934a4a",
-  lightest: "#c39a9a",
+  lightest: "#9f5e5e",
 };
 
 export const secondary: IColorTheme = {
-  darkest: "#003542",
-  dark: "#004F62",
-  mediumDark: "#266979",
-  medium: "#6695A1",
-  mediumLight: "#8CB0B8",
-  light: "#8CB0B8",
-  lightest: "#D9E5E8",
+  darkest: "#d0c3ae",
+  dark: "#e7d9c1",
+  mediumDark: "#ffefd5",
+  medium: "#fff3e0",
+  mediumLight: "#fff3e0",
+  light: "#fffaf3",
+  lightest: "#fffaf3",
 };
+
 export const neutral: IColorTheme = {
-  darkest: "#27323F",
-  dark: "#4E5864",
-  mediumDark: "#818D9D",
-  medium: "#B8C4CE",
-  mediumLight: "#D9DFE5",
-  light: "#F3F5F7",
-  lightest: "#FFFFFF",
+  darkest: "#1e2227",
+  dark: "#323a41",
+  mediumDark: "#515d68",
+  medium: "#657482",
+  mediumLight: "#97a2ae",
+  light: "#d8dce0",
+  lightest: "#f2f3f4",
 };
+
 export const info: IColorTheme = {
-  darkest: "#203D54",
-  dark: "#1A4971",
-  mediumDark: "#205E92",
-  medium: "#2368A2",
-  mediumLight: "#63A2D8",
-  light: "#AAD4F5",
-  lightest: "#EFF8FF",
+  darkest: "#1d2f66",
+  dark: "#29428f",
+  mediumDark: "#3555b8",
+  medium: "#4169e1",
+  mediumLight: "#6384e6",
+  light: "#869feb",
+  lightest: "#a8baf1",
 };
+
 export const warning: IColorTheme = {
-  darkest: "#AC5500",
-  dark: "#BB6711",
-  mediumDark: "#E08122",
-  medium: "#F29D49",
-  mediumLight: "#FFB366",
-  light: "#FFD1A2",
-  lightest: "#FFEBD7",
+  darkest: "#733f00",
+  dark: "#a25900",
+  mediumDark: "#d07200",
+  medium: "#ff8c00",
+  mediumLight: "#ffa02e",
+  light: "#ffb55c",
+  lightest: "#ffca8b",
 };
+
 export const danger: IColorTheme = {
-  darkest: "#891B1B",
-  dark: "#B82020",
-  mediumDark: "#DC3030",
-  medium: "#E46666",
-  mediumLight: "#F29E9E",
-  light: "#FBD3D3",
-  lightest: "#FCE8E8",
+  darkest: "#731f00",
+  dark: "#a22b00",
+  mediumDark: "#d03800",
+  medium: "#ff4500",
+  mediumLight: "#ff5517",
+  light: "#ff7745",
+  lightest: "#ff9973",
 };
+
 export const success: IColorTheme = {
-  darkest: "#135239",
-  dark: "#177741",
-  mediumDark: "#239D58",
-  medium: "#36C172",
-  mediumLight: "#73D99F",
-  light: "#A8EEC1",
-  lightest: "#E3FCEC",
+  darkest: "#30400f",
+  dark: "#445a16",
+  mediumDark: "#57741c",
+  medium: "#6b8e23",
+  mediumLight: "#789837",
+  light: "#93ac5f",
+  lightest: "#aec187",
 };
 
 export interface IColors {
