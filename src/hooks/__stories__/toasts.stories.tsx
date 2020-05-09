a62048da62c21985c78bf3d89875ac58f8157107
Sebastian Cabeza <sebastian.cabeza@noted.com>
2020-05-10 10:42:11 +1200

    Add toast stories

---
+++ src/hooks/__stories__/toasts.stories.tsx
@@ -0,0 +1,70 @@
+// tslint:disable: jsx-use-translation-function
+import * as React from "react";
+import { storiesOf } from "@storybook/react";
+import { Form, Label, Input, Box, Button, Shell, Toggle } from "../../components";
+import { useToast } from "..";
+import { styled } from "../..";
+
+storiesOf("Toasts", module).add("Toasts", () => <MyStory />);
+
+const Toast = styled(Box)`
+  margin-right: 16px;
+  margin-top: 16px;
+  width: 400px;
+  position: relative;
+  justify-content: space-between;
+  display: flex;
+  padding: 16px;
+  border: 1px solid #d7d7d7;
+  border-radius: 3px;
+  background: white;
+  box-shadow: 0px 4px 10px 0px #d7d7d7;
+  color: #494e5c;
+`;
+
+const MyStory = () => {
+  const { toast } = useToast();
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const content = e.currentTarget.content.value;
+    const persisting = !!e.currentTarget.persist.checked;
+    const timeout = persisting ? "persist" : e.currentTarget.timeout.value;
+    if (persisting) {
+      toast(
+        ({ close }) => (
+          <Toast>
+            {content} <Button onClick={() => close()}>Close</Button>
+          </Toast>
+        ),
+        { timeout }
+      );
+    } else {
+      toast(<Toast>{content}</Toast>, { timeout });
+    }
+  };
+
+  const [persist, setPersist] = React.useState(true);
+
+  return (
+    <Shell>
+      <Form margin="2" width="350px" display="grid" gridGap="2" onSubmit={handleSubmit}>
+        <Box>
+          <Label htmlFor="content">Content</Label>
+          <Input id="content" defaultValue={"Some toast content"} />
+        </Box>
+        <Box>
+          <Label htmlFor="timeout">Timeout</Label>
+          <Input id="timeout" type="number" defaultValue={3000} disabled={persist} />
+        </Box>
+        <Box>
+          <Label htmlFor="persist">Persist</Label>
+          <Toggle id="persist" defaultChecked={true} onChange={e => setPersist(e.target.checked)} />
+        </Box>
+        <Button variant="primary" type="submit">
+          Toast!
+        </Button>
+      </Form>
+    </Shell>
+  );
+};
