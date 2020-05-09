61f2a2d02d56f2575aabd6de6797d570e03ac74f
Sebastian Cabeza <sebastian.cabeza@noted.com>
2020-05-09 18:00:37 +1200

    Update storybook style import

--- .storybook/config.js
+++ .storybook/config.js
@@ -3,7 +3,7 @@ import { configure, addDecorator } from "@storybook/react";
 import { UiProvider } from "../src/theme/ui-provider";
 import { useDarkMode } from "storybook-dark-mode";
 import { addParameters } from "@storybook/react";
-import "../src/public/style.css";
+import "../assets/style.css";
 
 const req = require.context("../src", true, /\.stories\.(ts|tsx)$/);
 
