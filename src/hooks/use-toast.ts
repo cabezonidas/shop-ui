3268db8d1dc40b4fd1fefe016a60006c051631ed
Sebastian Cabeza <sebastian.cabeza@noted.com>
2020-05-10 15:31:14 +1200

    Rename and add variants

--- src/hooks/use-toast.ts
+++
@@ -1,4 +0,0 @@
-import * as React from "react";
-import { ToastContext } from "../theme/toast-provider";
-
-export const useToast = () => React.useContext(ToastContext);
---
+++ src/hooks/use-toast.tsx
@@ -0,0 +1,43 @@
+import * as React from "react";
+import { ToastContext } from "../theme/toast-provider";
+import styled from "@emotion/styled";
+import { Button, Box } from "..";
+import { Close } from "../icons";
+
+const Toast = styled(Button.withComponent("div"))(({ theme }) => ({
+  minWidth: 300,
+  display: "flex",
+  justifyContent: "space-between",
+  alignItems: "center",
+  marginRight: theme.space[5],
+  marginBottom: theme.space[5],
+  padding: theme.space[5],
+  borderRadius: theme.space[2],
+  // boxShadow: `0px 4px 10px 0px ${theme.colors.neutral.dark} !important`,
+}));
+
+type ToastVariant = "primary" | "secondary" | "default" | "info" | "warning" | "danger";
+
+export const useToast = () => {
+  const { toast } = React.useContext(ToastContext);
+
+  const notify = (message: string, variant: ToastVariant = "primary") => {
+    if (variant !== "danger") {
+      toast(<Toast variant={variant}>{message}</Toast>, { timeout: 3000 });
+    } else {
+      toast(
+        e => (
+          <Toast variant={variant}>
+            <Box>{message}</Box>
+            <Button variant="transparent" onClick={() => e.close()} color="inherit !important">
+              <Close width="12" height="12" />
+            </Button>
+          </Toast>
+        ),
+        { timeout: "persist" }
+      );
+    }
+  };
+
+  return { toast, notify };
+};
