45084f06ce05447fe8165ed495882bbf03d6a0a6
Sebastian Cabeza <sebastian.cabeza@noted.com>
2020-05-10 17:57:19 +1200

    Add toast positioning

--- src/hooks/use-toast.tsx
+++ src/hooks/use-toast.tsx
@@ -1,39 +1,55 @@
 import * as React from "react";
-import { ToastContext } from "../theme/toast-provider";
+import { ToastContext, IPosition } from "../theme/toast-provider";
 import styled from "@emotion/styled";
 import { Button, Box } from "..";
 import { Close } from "../icons";
 
 const Toast = styled(Button.withComponent("div"))(({ theme }) => ({
-  minWidth: 300,
+  width: 300,
+  maxWidth: "90%",
+  overflow: "hidden",
+  whiteSpace: "nowrap",
   display: "flex",
   justifyContent: "space-between",
-  alignItems: "center",
-  marginRight: theme.space[5],
-  marginBottom: theme.space[5],
+  alignItems: "inherit",
+  marginBottom: theme.space[2],
   padding: theme.space[5],
   borderRadius: theme.space[2],
 }));
 
 type ToastVariant = "primary" | "secondary" | "default" | "info" | "warning" | "danger";
 
+interface INotifyOptions {
+  variant?: ToastVariant;
+  position?: IPosition;
+}
+
 export const useToast = () => {
   const { toast } = React.useContext(ToastContext);
 
-  const notify = (message: string, variant: ToastVariant = "primary") => {
+  const notify = (message: string, options: INotifyOptions) => {
+    const variant = options?.variant ?? "primary";
+    const position = options?.position ?? "bottom-right";
     if (variant !== "danger") {
-      toast(<Toast variant={variant}>{message}</Toast>, { timeout: 3000 });
+      toast(
+        e => (
+          <Toast variant={variant} onClick={() => e.close()}>
+            {message}
+          </Toast>
+        ),
+        { timeout: 3000, position }
+      );
     } else {
       toast(
         e => (
-          <Toast variant={variant}>
+          <Toast variant={variant} onClick={() => e.close()}>
             <Box>{message}</Box>
-            <Button variant="transparent" onClick={() => e.close()} color="inherit !important">
+            <Button variant="transparent" color="inherit !important">
               <Close width="12" height="12" />
             </Button>
           </Toast>
         ),
-        { timeout: "persist" }
+        { timeout: "persist", position }
       );
     }
   };
--- src/theme/toast-provider.tsx
+++ src/theme/toast-provider.tsx
@@ -11,8 +11,17 @@ interface IToast {
   options?: IToastOptions;
 }
 
+export type IPosition =
+  | "top-left"
+  | "top-center"
+  | "top-right"
+  | "bottom-left"
+  | "bottom-center"
+  | "bottom-right";
+
 export interface IToastOptions {
   timeout?: number | "persist";
+  position?: IPosition;
 }
 
 type ToastChildren = React.ReactNode | ((e: { close: () => void }) => React.ReactNode);
@@ -24,12 +33,10 @@ export const ToastContext = React.createContext<{
 const ToastContainer = styled(Box)(({ theme }) => ({
   position: "absolute",
   zIndex: 1,
-  bottom: 0,
-  right: 0,
   overflow: "hidden",
+  padding: theme.space[3],
   display: "flex",
   flexDirection: "column",
-  padding: theme.space[1],
 }));
 
 const timeout = (defaultTimeout: number, options?: IToastOptions) =>
@@ -51,6 +58,41 @@ function takeLast<T>(arr: T, elements: number): T {
   return (res as unknown) as T;
 }
 
+const reduceToasts = (toasts: IToast[]) =>
+  toasts.reduce(
+    (res, item) => {
+      const position = item?.options.position ?? "bottom-right";
+      switch (position) {
+        case "top-left":
+          return { ...res, topLeft: [...res.topLeft, item] };
+        case "top-center":
+          return { ...res, topCenter: [...res.topCenter, item] };
+        case "top-right":
+          return { ...res, topRight: [...res.topRight, item] };
+        case "bottom-left":
+          return { ...res, bottomLeft: [...res.bottomLeft, item] };
+        case "bottom-center":
+          return { ...res, bottomCenter: [...res.bottomCenter, item] };
+        case "bottom-right":
+          return { ...res, bottomRight: [...res.bottomRight, item] };
+      }
+    },
+    {
+      topLeft: [] as IToast[],
+      topCenter: [] as IToast[],
+      topRight: [] as IToast[],
+      bottomLeft: [] as IToast[],
+      bottomCenter: [] as IToast[],
+      bottomRight: [] as IToast[],
+    }
+  );
+
+const transition = (place: "left" | "right" | "top" | "bottom") => ({
+  from: { [place]: "-100%" },
+  enter: { [place]: "0%" },
+  leave: { [place]: "-100%" },
+});
+
 export const ToastState: React.FC<{ defaultTimeout?: number; stack?: number }> = ({
   children,
   defaultTimeout = 3000,
@@ -68,33 +110,62 @@ export const ToastState: React.FC<{ defaultTimeout?: number; stack?: number }> =
     [setToasts, key.current, stack]
   );
 
-  const transitions = useTransition(toasts, {
-    from: { right: "-100%" },
-    enter: { right: "0%" },
-    leave: { right: "-100%" },
-  });
+  const reducedToasts = reduceToasts(toasts);
 
-  console.log(transitions);
+  const topLeft = useTransition(reducedToasts.topLeft, transition("left"));
+  const topCenter = useTransition(reducedToasts.topCenter, transition("top"));
+  const topRight = useTransition(reducedToasts.topRight, transition("right"));
+  const bottomLeft = useTransition(reducedToasts.bottomLeft, transition("left"));
+  const bottomCenter = useTransition(reducedToasts.bottomCenter, transition("bottom"));
+  const bottomRight = useTransition(reducedToasts.bottomRight, transition("right"));
 
   return (
     <ToastContext.Provider value={{ toast }}>
       {createPortal(
-        <ToastContainer>
-          {transitions((style, t) => (
-            <AnimatedToast key={t.id} style={style} ml="auto">
-              <Toast
-                removeToast={() => setToasts(ts => ts.filter(n => n.id !== t.id))}
-                expiry={timeout(defaultTimeout, t.options)}
-              >
-                {typeof t.notification === "function"
-                  ? t.notification({
-                      close: () => setToasts(tts => tts.filter(tt => tt.id !== t.id)),
-                    })
-                  : t.notification}
-              </Toast>
-            </AnimatedToast>
-          ))}
-        </ToastContainer>,
+        <>
+          <ToastContainer top={0} right={0}>
+            {topRight((style, t) => (
+              <AnimatedToast key={t.id} style={{ ...style, alignSelf: "flex-end" }}>
+                <InnerToast {...{ t, setToasts, defaultTimeout }} />
+              </AnimatedToast>
+            ))}
+          </ToastContainer>
+          <ToastContainer top={0} left={0} right={0}>
+            {topCenter((style, t) => (
+              <AnimatedToast key={t.id} style={{ ...style, alignSelf: "center" }}>
+                <InnerToast {...{ t, setToasts, defaultTimeout }} />
+              </AnimatedToast>
+            ))}
+          </ToastContainer>
+          <ToastContainer top={0} left={0}>
+            {topLeft((style, t) => (
+              <AnimatedToast key={t.id} style={{ ...style, alignSelf: "flex-start" }}>
+                <InnerToast {...{ t, setToasts, defaultTimeout }} />
+              </AnimatedToast>
+            ))}
+          </ToastContainer>
+          <ToastContainer bottom={0} right={0}>
+            {bottomRight((style, t) => (
+              <AnimatedToast key={t.id} style={{ ...style, alignSelf: "flex-end" }}>
+                <InnerToast {...{ t, setToasts, defaultTimeout }} />
+              </AnimatedToast>
+            ))}
+          </ToastContainer>
+          <ToastContainer bottom={0} right={0} left={0}>
+            {bottomCenter((style, t) => (
+              <AnimatedToast key={t.id} style={{ ...style, alignSelf: "center" }}>
+                <InnerToast {...{ t, setToasts, defaultTimeout }} />
+              </AnimatedToast>
+            ))}
+          </ToastContainer>
+          <ToastContainer bottom={0} left={0}>
+            {bottomLeft((style, t) => (
+              <AnimatedToast key={t.id} style={{ ...style, alignSelf: "flex-start" }}>
+                <InnerToast {...{ t, setToasts, defaultTimeout }} />
+              </AnimatedToast>
+            ))}
+          </ToastContainer>
+        </>,
         document.body
       )}
       {children}
@@ -104,11 +175,31 @@ export const ToastState: React.FC<{ defaultTimeout?: number; stack?: number }> =
 
 ToastState.displayName = "ToastState";
 
+const InnerToast: React.FC<{
+  t: IToast;
+  setToasts: React.Dispatch<React.SetStateAction<IToast[]>>;
+  defaultTimeout: number;
+}> = props => {
+  const { t, setToasts, defaultTimeout } = props;
+  return (
+    <ToastTimeout
+      removeToast={() => setToasts(ts => ts.filter(n => n.id !== t.id))}
+      expiry={timeout(defaultTimeout, t.options)}
+    >
+      {typeof t.notification === "function"
+        ? t.notification({
+            close: () => setToasts(tts => tts.filter(tt => tt.id !== t.id)),
+          })
+        : t.notification}
+    </ToastTimeout>
+  );
+};
+
 interface IToastProps {
   removeToast: () => void;
   expiry?: number;
 }
-const Toast: React.FC<IToastProps> = props => {
+const ToastTimeout: React.FC<IToastProps> = props => {
   const { removeToast, expiry, children } = props;
   React.useEffect(() => {
     if (expiry !== undefined) {
@@ -123,6 +214,6 @@ const Toast: React.FC<IToastProps> = props => {
 
   return <>{children}</>;
 };
-Toast.displayName = "Toast";
+ToastTimeout.displayName = "ToastTimeout";
 
 const AnimatedToast = animated(Box);
