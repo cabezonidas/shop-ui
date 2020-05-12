82e70972b7e62540d7f0df9ac986c50417fcf75a
Sebastian Cabeza <sebastian.cabeza@noted.com>
2020-05-13 09:19:57 +1200

    Add alerts

---
+++ src/components/__stories__/alert.stories.tsx
@@ -0,0 +1,46 @@
+// tslint:disable: jsx-use-translation-function
+import * as React from "react";
+import { storiesOf } from "@storybook/react";
+import { Form, Label, Input, Box, File, Checkbox, Select, Option, Toggle } from "..";
+import Alert from "../alert";
+
+storiesOf("Alerts", module).add("Variants", () => <MyStory />);
+
+const MyStory = () => {
+  return (
+    <Form margin="2" width="350px">
+      <Box>
+        <Label htmlFor="input">Input</Label>
+        <Input id="input" onChange={e => console.log(e)} />
+        <Alert variant="primary">This is an alert</Alert>
+      </Box>
+      <Box>
+        <Label htmlFor="select">Select</Label>
+        <Select id="select">
+          <Option value="1">Option 1</Option>
+          <Option value="2">Option 2</Option>
+        </Select>
+        <Alert variant="default">This is an alert</Alert>
+      </Box>
+      <Box>
+        <Label htmlFor="checkbox">Checkbox</Label>
+        <Checkbox id="checkbox" onChange={e => console.log(e)} />
+        <Alert variant="info">This is an alert</Alert>
+      </Box>
+      <Box>
+        <Label htmlFor="file">File</Label>
+        <File id="file" onChange={e => console.log(e)} />
+        <Alert variant="warning">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut repudiandae eum, nisi animi
+          cupiditate ad? Voluptas assumenda natus nostrum corporis quo autem ipsa maxime possimus
+          quis, asperiores, earum repudiandae necessitatibus!
+        </Alert>
+      </Box>
+      <Box>
+        <Label htmlFor="toggle">Toggle</Label>
+        <Toggle id="toggle" onChange={e => console.log(e)} />
+        <Alert variant="danger">This is an alert</Alert>
+      </Box>
+    </Form>
+  );
+};
---
+++ src/components/alert.tsx
@@ -0,0 +1,34 @@
+import { Alert as ReachAlert } from "@reach/alert";
+import { Box } from ".";
+import { IColorTheme } from "../theme/colors";
+import { styled } from "../theme";
+
+type AlertVariant = "primary" | "info" | "warning" | "danger" | "default";
+
+export const Alert = styled(Box.withComponent(ReachAlert))<{ variant?: AlertVariant }>(
+  ({ theme, variant = "danger" }) => {
+    const { colors, mode } = theme;
+
+    const colorGrade = colors[variant === "default" ? "neutral" : variant] as IColorTheme;
+
+    return {
+      textOverflow: "ellipsis",
+      appearance: "none",
+      display: "inline-block",
+      verticalAlign: "middle",
+      WebkitFontSmoothing: "subpixel-antialiased",
+      transition: `border-color 0.25s ease-in-out, box-shadow 0.1s ease-in-out background-color 0.25s ease-in-out, color 0.25s ease-in-out;`,
+      padding: `${theme.space[2]} ${theme.space[4]}`,
+      backgroundColor: `${colorGrade.lightest}${mode === "dark" ? 80 : 20}`,
+      color: mode === "light" ? colorGrade.darkest : colors.neutral.lightest,
+      outlineColor: colorGrade.medium,
+      borderRadius: theme.space[0],
+      width: "100%",
+      marginTop: theme.space[1],
+    };
+  }
+);
+
+Alert.displayName = "Alert";
+
+export default Alert;
