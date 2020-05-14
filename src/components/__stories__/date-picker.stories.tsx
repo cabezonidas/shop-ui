1e9bcf33c140c927445196787ea25676ddb725d4
Sebastian Cabeza <sebastian.cabeza@noted.com>
2020-05-14 18:30:41 +1200

    Add story with disabled

--- src/components/__stories__/date-picker.stories.tsx
+++ src/components/__stories__/date-picker.stories.tsx
@@ -2,22 +2,33 @@
 import * as React from "react";
 import { storiesOf } from "@storybook/react";
 import { Form, Label, Box, DatePicker, Calendar } from "..";
-import { DateTime } from "luxon";
+import { DateTime, Interval } from "luxon";
 
 storiesOf("DatePicker", module).add("Variants", () => <MyStory />);
 
 const MyStory = () => {
   const [day1, setDay1] = React.useState(DateTime.local());
   const [day2, setDay2] = React.useState<DateTime>();
+  const [day3, setDay3] = React.useState(DateTime.local());
+  const allowedIntervals = [Interval.after(day1.minus({ days: 5 }), { days: 10 })];
   return (
     <Form margin="2" width="650px" display="grid" gridGap="2">
       <Box>
-        <Label htmlFor="date">Date</Label>
-        <DatePicker id="date" day={day1} onDaySelect={setDay1} />
+        <Label htmlFor="date1">Date</Label>
+        <DatePicker id="date1" day={day1} onDaySelect={setDay1} />
       </Box>
       <Box>
-        <Label htmlFor="date-time">Date and time</Label>
-        <DatePicker id="date-time" day={day2} onDaySelect={setDay2} />
+        <Label htmlFor="date2">Date (empty)</Label>
+        <DatePicker id="date2" day={day2} onDaySelect={setDay2} />
+      </Box>
+      <Box>
+        <Label htmlFor="date3">Allowed Intervals</Label>
+        <DatePicker
+          id="date3"
+          day={day3}
+          onDaySelect={setDay3}
+          allowedIntervals={allowedIntervals}
+        />
       </Box>
     </Form>
   );
