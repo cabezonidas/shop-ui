6e0233bd0c374b56fc60b6430cd29d4e3cba2ddf
Sebastian Cabeza <sebastian.cabeza@noted.com>
2020-06-24 19:22:43 +1200

    Add tests

--- src/utils/parse-markdown-images.test.ts
+++ src/utils/parse-markdown-images.test.ts
@@ -1,20 +1,26 @@
-import { parseMarkdownImages } from "./parse-markdown-images";
+import { parseMarkdownImages, listMarkdownImages } from "./parse-markdown-images";
 
 const fakeHtml = `
 <div>
 <img src="https://cabezonidas-shop-photos.s3.amazonaws.com/friends/picture1.png" />
-<img src="https://cabezonidas-shop-photos.s3.amazonaws.com/friends/picture2.png" />
+<img src='https://cabezonidas-shop-photos.s3.amazonaws.com/friends/picture2.png' />
 <div>
 `;
 const fakeHtmlParsed = `
 <div>
 <img src="https://ik.imagekit.io/syuhz8bmxl/friends/picture1.png?tr=w-600px" />
-<img src="https://ik.imagekit.io/syuhz8bmxl/friends/picture2.png?tr=w-600px" />
+<img src='https://ik.imagekit.io/syuhz8bmxl/friends/picture2.png?tr=w-600px' />
 <div>
 `;
 
 describe("markdown parser", () => {
   it("converts aws images to imagekit cdn", () => {
     expect(parseMarkdownImages(fakeHtml)).toBe(fakeHtmlParsed);
+  });
+  it("gets all the images", () => {
+    const images = listMarkdownImages(fakeHtml);
+    expect(images[0]).toBe("https://cabezonidas-shop-photos.s3.amazonaws.com/friends/picture1.png");
+    expect(images[1]).toBe("https://cabezonidas-shop-photos.s3.amazonaws.com/friends/picture2.png");
+    expect(images.length).toBe(2);
   });
 });
