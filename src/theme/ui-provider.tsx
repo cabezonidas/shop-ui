2354b1503824449d4805f3ac91ed70729a5db3fe
Sebastian Cabeza <sebastian.cabeza@noted.com>
2020-06-27 22:19:29 +1200

    Fix translation provider to be able to run server side

--- src/theme/ui-provider.tsx
+++ src/theme/ui-provider.tsx
@@ -10,20 +10,13 @@ import { ToastProvider } from "./toast/toast";
 import { DarkModeState, DarkModeContext } from "./dark-mode";
 import { IColorsProp, colorsDollar } from "./colors";
 
-const locales = ["es-AR", "en-US"] as ["es-AR", "en-US"];
-
-const initialLanguage =
-  locales.find(l => localStorage?.getItem("language") === l) ||
-  locales.find(l => l.startsWith((window?.navigator?.language ?? "").slice(0, 2))) ||
-  "en-US";
-
 i18next.use(initReactI18next).init({
   resources: {
     "en-US": { translation: {} },
     "es-AR": { translation: {} },
   },
-  lng: initialLanguage,
-  fallbackLng: initialLanguage,
+  lng: "en-US",
+  fallbackLng: "en-US",
 });
 
 export const UiProvider: React.FC<{
